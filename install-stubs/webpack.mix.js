b93d7d3d19663b26eae5c4fefdcbcf7e1f0c6067
Mohamed Said <themohamedsaid@gmail.com>
2018-01-09 21:55:44 +0200

    spark aurelius naming

--- install-stubs/webpack.mix.js
+++ install-stubs/webpack.mix.js
@@ -25,7 +25,7 @@ mix
     .webpackConfig({
         resolve: {
             modules: [
-                path.resolve(__dirname, 'vendor/laravel/spark/resources/assets/js'),
+                path.resolve(__dirname, 'vendor/laravel/spark-aurelius/resources/assets/js'),
                 'node_modules'
             ],
             alias: {
