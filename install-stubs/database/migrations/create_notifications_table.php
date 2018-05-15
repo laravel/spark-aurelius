50a18fb6c8fb527f2a310fe81cabce33e2c3dfd3
Ed Preston <epreston@prestonsoft.com>
2018-05-15 22:29:07 +1000

    matching foreign keys

--- install-stubs/database/migrations/create_notifications_table.php
+++ install-stubs/database/migrations/create_notifications_table.php
@@ -14,8 +14,8 @@ class CreateNotificationsTable extends Migration
     {
         Schema::create('notifications', function (Blueprint $table) {
             $table->string('id')->primary();
-            $table->integer('user_id');
-            $table->integer('created_by')->nullable();
+            $table->unsignedInteger('user_id');
+            $table->unsignedInteger('created_by')->nullable();
             $table->string('icon', 50)->nullable();
             $table->text('body');
             $table->string('action_text')->nullable();
