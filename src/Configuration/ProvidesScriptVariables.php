d33afa6d628d014244c73378f63eec441514d45b
Mohamed Said <themohamedsaid@gmail.com>
2018-11-17 19:20:00 +0200

    fix

--- src/Configuration/ProvidesScriptVariables.php
+++ src/Configuration/ProvidesScriptVariables.php
@@ -18,7 +18,7 @@ trait ProvidesScriptVariables
     public static function scriptVariables()
     {
         return [
-//            'translations' => static::getTranslations() + ['teams.team' => trans('teams.team'), 'teams.member' => trans('teams.member')],
+            'translations' => static::getTranslations() + ['teams.team' => trans('teams.team'), 'teams.member' => trans('teams.member')],
             'braintreeMerchantId' => config('services.braintree.merchant_id'),
             'braintreeToken' => Spark::needsBraintreeToken(request()) && Spark::billsUsingBraintree() ? BraintreeClientToken::generate() : null,
             'cardUpFront' => Spark::needsCardUpFront(),
