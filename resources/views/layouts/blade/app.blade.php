f750e20da8b9a6a302f5c33850a12b21ef7cf5fd
Alan Wynn <alan.wynn@me.com>
2018-11-27 20:07:11 +0000

    Add CSRF Meta tag to app.blade layout
    Signed-off-by: Alan Wynn <alan.wynn@me.com>

--- resources/views/layouts/blade/app.blade.php
+++ resources/views/layouts/blade/app.blade.php
@@ -3,6 +3,7 @@
 <head>
     <!-- Meta Information -->
     <meta charset="utf-8">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
