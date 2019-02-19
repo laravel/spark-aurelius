d83fe4a63de0de42e762107f7fb3f76926e382ff
Adnan RIHAN <adnan@rihan.fr>
2019-02-19 23:12:33 +0100

    Updated missing keys and files for FR lang

---
+++ install-stubs/resources/lang/fr/auth.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Lignes de traduction d'"Authentification"
+    |--------------------------------------------------------------------------
+    |
+    | Les lignes suivantes sont utilisées pour afficher différents messages
+    | à l'utilisateur, en rapport avec l'authentification.
+    | N'hésitez pas à personnaliser chacun des messages ci-dessous.
+    |
+    */
+
+    'failed' => 'Ces identifiants ne correspondent pas à nos enregistrements.',
+    'throttle' => 'Trop de tentatives de connexion. Veuillez réessayer dans :seconds secondes.',
+
+];
