411df03bfda5920558876833989bc1879faa690d
Andrew <browner12@gmail.com>
2019-03-01 12:32:24 -0600

    Update webpack.mix.js
    only create RTL css and version files when running in production
    
    in local development, there is a lot of iteration which leads to a lot of code compilation. this can often be a timely operation, which can be a point of frustration.
    
    by moving this code to run only in production, I have seen reductions of 5 to 10 seconds in compilations.

--- install-stubs/webpack.mix.js
+++ install-stubs/webpack.mix.js
@@ -17,11 +17,6 @@ mix
     .sass('resources/sass/app.scss', 'public/css')
     .js('resources/js/app.js', 'public/js')
     .copy('node_modules/sweetalert/dist/sweetalert.min.js', 'public/js/sweetalert.min.js')
-    .sass('resources/sass/app-rtl.scss', 'public/css')
-    .then(() => {
-        exec('node_modules/rtlcss/bin/rtlcss.js public/css/app-rtl.css ./public/css/app-rtl.css');
-    })
-    .version()
     .webpackConfig({
         resolve: {
             modules: [
@@ -33,3 +28,13 @@ mix
             }
         }
     });
+
+if (mix.inProduction()) {
+
+    mix
+        .sass('resources/sass/app-rtl.scss', 'public/css')
+        .then(() => {
+            exec('node_modules/rtlcss/bin/rtlcss.js public/css/app-rtl.css ./public/css/app-rtl.css');
+        })
+        .version();
+}
