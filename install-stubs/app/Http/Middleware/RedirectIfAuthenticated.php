b1a590a398aac2832dc3bf7244c4f6c2c701b84f
Marc Newton <php@marcnewton.info>
2019-05-03 10:53:49 +0100

    Fix RedirectIfAuthenticated redirect value
    Middleware redirect uses static string '/home' instead of retrieving redirect value from ManagesAppOptions using Spark::afterLoginRedirect()

--- install-stubs/app/Http/Middleware/RedirectIfAuthenticated.php
+++ install-stubs/app/Http/Middleware/RedirectIfAuthenticated.php
@@ -4,6 +4,7 @@ namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Support\Facades\Auth;
+use Laravel\Spark\Spark;
 
 class RedirectIfAuthenticated
 {
@@ -18,7 +19,7 @@ class RedirectIfAuthenticated
     public function handle($request, Closure $next, $guard = null)
     {
         if (Auth::guard($guard)->check()) {
-            return redirect('/home');
+            return redirect(Spark::afterLoginRedirect());
         }
 
         return $next($request);
