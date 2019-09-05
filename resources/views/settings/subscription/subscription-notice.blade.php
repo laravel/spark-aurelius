bf6ce3e220fe0498ee5e45c436b1d3ea9de24e85
Mohamed Said <themohamedsaid@gmail.com>
2019-09-05 11:50:39 +0200

    fix subscription-notice

--- resources/views/settings/subscription/subscription-notice.blade.php
+++ resources/views/settings/subscription/subscription-notice.blade.php
@@ -2,7 +2,7 @@
     <div class="alert alert-warning  mb-4">
         {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->subscription()->latestPayment()->id.'?redirect=/home">', 'linkClose' => '</a>']) !!}
     </div>
-@elseif(auth()->user()->currentTeam() && auth()->user()->currentTeam()->subscription() && in_array(auth()->user()->currentTeam()->subscription()->stripe_status, ['incomplete' , 'past_due']))
+@elseif(method_exists(auth()->user(), 'currentTeam') && auth()->user()->currentTeam() && auth()->user()->currentTeam()->subscription() && in_array(auth()->user()->currentTeam()->subscription()->stripe_status, ['incomplete' , 'past_due']))
     <div class="alert alert-warning  mb-4">
         {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->currentTeam()->subscription()->latestPayment()->id.'?redirect=/home">', 'linkClose' => '</a>']) !!}
     </div>
