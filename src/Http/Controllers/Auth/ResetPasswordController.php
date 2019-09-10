5f6071b77443a9255b773965c0ea2140b12fed91
Samuel De Backer <sdebacker@gmail.com>
2019-09-10 23:27:30 +0200

    Create ResetPasswordController.php

---
+++ src/Http/Controllers/Auth/ResetPasswordController.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Laravel\Spark\Http\Controllers\Auth;
+
+use Illuminate\Foundation\Auth\ResetsPasswords;
+use Illuminate\Http\Request;
+use Laravel\Spark\Http\Controllers\Controller;
+use Laravel\Spark\Spark;
+
+class ResetPasswordController extends Controller
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Password Reset Controller
+    |--------------------------------------------------------------------------
+    |
+    | This controller is responsible for handling password reset requests
+    | and uses a simple trait to include this behavior. You're free to
+    | explore this trait and override any methods you wish to tweak.
+    |
+    */
+
+    use ResetsPasswords;
+
+    /**
+     * Where to redirect users after resetting their password.
+     *
+     * @var string
+     */
+    protected $redirectTo = '/home';
+
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        $this->middleware('guest');
+
+        $this->redirectTo = Spark::afterLoginRedirect();
+    }
+
+    /**
+     * Display the password reset view for the given token.
+     *
+     * If no token is present, display the link request form.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  string|null  $token
+     * @return \Illuminate\Http\Response
+     */
+    public function showResetForm(Request $request, $token = null)
+    {
+        return view('spark::auth.passwords.reset')->with(
+            ['token' => $token, 'email' => $request->email]
+        );
+    }
+}
