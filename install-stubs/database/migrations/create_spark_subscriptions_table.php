19e88c9e193eb91e548feab8d9b76e8aaa37dccb
Mohamed Said <themohamedsaid@gmail.com>
2020-04-22 16:55:34 +0200

    support cashier 11

--- install-stubs/database/migrations/create_spark_subscriptions_table.php
+++ install-stubs/database/migrations/create_spark_subscriptions_table.php
@@ -17,9 +17,9 @@ class CreateSparkSubscriptionsTable extends Migration
             $table->unsignedBigInteger('user_id');
             $table->string('name');
             $table->string('stripe_id');
-            $table->string('stripe_plan');
+            $table->string('stripe_plan')->nullable();
             $table->string('stripe_status')->nullable();
-            $table->integer('quantity');
+            $table->integer('quantity')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
             $table->timestamp('ends_at')->nullable();
             $table->timestamps();
@@ -30,12 +30,34 @@ class CreateSparkSubscriptionsTable extends Migration
             $table->unsignedBigInteger('team_id');
             $table->string('name');
             $table->string('stripe_id');
-            $table->string('stripe_plan');
+            $table->string('stripe_plan')->nullable();
             $table->string('stripe_status')->nullable();
-            $table->integer('quantity');
+            $table->integer('quantity')->nullable();
             $table->timestamp('trial_ends_at')->nullable();
             $table->timestamp('ends_at')->nullable();
             $table->timestamps();
+        });
+
+        Schema::create('subscription_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('subscription_id');
+            $table->string('stripe_id')->index();
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'stripe_plan']);
+        });
+
+        Schema::create('team_subscription_items', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('subscription_id');
+            $table->string('stripe_id')->index();
+            $table->string('stripe_plan');
+            $table->integer('quantity');
+            $table->timestamps();
+
+            $table->unique(['subscription_id', 'stripe_plan']);
         });
     }
 
