9350c0212a43522aefcdc501dea7887ea1603bb2
Mohamed Said <themohamedsaid@gmail.com>
2020-07-16 21:22:04 +0200

    update redirect urls

--- resources/views/settings/subscription/subscription-notice.blade.php
+++ resources/views/settings/subscription/subscription-notice.blade.php
@@ -1,10 +1,10 @@
 @if(auth()->user()->subscription() && in_array(auth()->user()->subscription()->stripe_status, ['incomplete' , 'past_due']))
     <div class="alert alert-warning  mb-4">
-        {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->subscription()->latestPayment()->id.'?redirect=/home">', 'linkClose' => '</a>']) !!}
+        {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->subscription()->latestPayment()->id.'?redirect='.url('/home').'">', 'linkClose' => '</a>']) !!}
     </div>
 @elseif(method_exists(auth()->user(), 'currentTeam') && auth()->user()->currentTeam() && auth()->user()->currentTeam()->subscription() && in_array(auth()->user()->currentTeam()->subscription()->stripe_status, ['incomplete' , 'past_due']))
     <div class="alert alert-warning  mb-4">
-        {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->currentTeam()->subscription()->latestPayment()->id.'?redirect=/home">', 'linkClose' => '</a>']) !!}
+        {!! __('Please :linkOpen confirm your payment :linkClose to activate your subscription!', ['linkOpen' => '<a href="/'.config('cashier.path').'/payment/'.auth()->user()->currentTeam()->subscription()->latestPayment()->id.'?redirect='.url('/home').'">', 'linkClose' => '</a>']) !!}
     </div>
 @else
     <div class="alert alert-warning mb-4" v-if="subscriptionIsOnTrial">
